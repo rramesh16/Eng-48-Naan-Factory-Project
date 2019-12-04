f9c65a61b0de2e607c7274aa3db22a4e50eea224
Rahavi Ramesh <RRamesh@spartaglobal.com>
2019-12-04 15:09:05 +0000

    addded two more tests

--- naan_factory.py
+++ naan_factory.py
@@ -3,17 +3,27 @@
 # Having controlled inputs for known outputs and testing
 
 def make_dough(arg1, arg2):
-    pass
+    return 'dough'
 
 def bake_dough(arg1):
-    pass
+    return 'Naan'
+
+
 
 # Make test for make_dough
 print("Testing make_dough with 'water' and 'flour'. Expected --> 'dough'")
 print(make_dough('water', 'flour') == 'dough')
 print('got:', make_dough('water', 'flour'))
 
+#When I pass in cement and water or any other arguement...I should get: 'Not dough'
+
+make_dough('cement', 'cement') == 'Not dough'
+
 # Make test for bake_dough
 print("Testing bake_dough with 'dough'. Expected --> 'Naan'")
 print(bake_dough('dough') == 'Naan')
-print('got:', bake_dough('dough'))
+print("When calling bake dough('dough'), got:", bake_dough('dough'))
+
+# When bake_dough is passed something other than 'dough' it should output: 'not Naan'
+
+bake_dough('chicken') == 'Not Naan'
